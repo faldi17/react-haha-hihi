43729bca7846740f1885f3b17ec4b69a66522497
Faldiansyah <faldiansyahk@gmail.com>
2025-02-11 00:00:59 +0700

    initial commit

---
+++ dom-mnp/script.js
@@ -0,0 +1,6 @@
+const app = document.getElementById('app');
+const heading = document.createElement('h1');
+const headingContent = document.createTextNode('Belajar React Nih Guys... 🚀');
+
+heading.appendChild(headingContent);
+app.appendChild(heading);
