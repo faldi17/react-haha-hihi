a15778ad2d9662469c25a233a3c0cdf6495414bc
Faldiansyah <faldiansyahk@gmail.com>
2025-02-11 00:49:52 +0700

    jsx rules

--- react-local/script.js
+++ react-local/script.js
@@ -1,3 +1,15 @@
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(<h1>Belajar React Nih Senggol Dong 🤣</h1>);
+root.render(
+    /*
+        3 ATURAN JSX
+        - Selalu mengembalikan satu elemen: bisa pake tag <></> buat render jadi satu elemen
+        - Selalu tutup semua tag: kayak <img />
+        - Gunakan camelCase untuk atribut: misal class jadi className
+    */
+    <>
+        <h1 className="heading">Belajar React Nih Senggol Dong 🤣</h1>
+        <img src="img/react-logo.png" width="80" alt="React Logo" />
+        <p onClick={e => {alert('oke')}}>Belajar React Itu Mudah Dan Menyenangkan Bukan ?!</p>
+    </>
+);
